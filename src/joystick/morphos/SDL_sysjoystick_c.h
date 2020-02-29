2190cdfc47cd55c373b63d1916e92ee95f401a11
BeWorld <36823759+BeWorld2018@users.noreply.github.com>
2020-02-29 22:41:52 +0100

    MORPHOS : fix with AROS_ALMOST_COMPATIBLE

--- src/joystick/morphos/SDL_sysjoystick_c.h
+++ src/joystick/morphos/SDL_sysjoystick_c.h
@@ -35,21 +35,6 @@ typedef signed int int32;
 #define MAX_BUTTONS         16
 #define MAX_HATS            8
 
-
-// Dont know why i need to redefine this...
-#define ForeachNode(l, n) 	\
-for (  \
-	n = (void *)(((struct List *)(l))->lh_Head);  \
-	((struct Node *)(n))->ln_Succ;  \
-	n = (void *)(((struct Node *)(n))->ln_Succ)  \
-)
-#define ForeachNodeSafe(l,n,n2)  \
-for (  \
-	n = (void *)(((struct List *)(l))->lh_Head);  \
-	(n2 = (void *)((struct Node *)(n))->ln_Succ);  \
-	n = (void *)n2  \
-)
-
 struct sensordata
 {
 	struct MsgPort *sensorport;
