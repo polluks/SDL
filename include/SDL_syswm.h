ae749cf083eea38410e96c17dc9fb2833e85bc56
BeWorld <36823759+BeWorld2018@users.noreply.github.com>
2020-02-29 22:23:54 +0100

    MORPHOS : mistake fix

--- include/SDL_syswm.h
+++ include/SDL_syswm.h
@@ -287,7 +287,12 @@ struct SDL_SysWMinfo
             struct Window *window;      /**< The AmigaOS 4 window */
         } os4;
 #endif
-
+#if defined(SDL_VIDEO_DRIVER_AMIGA)
+        struct
+        {
+            struct Window *window;      /**< Intuition window */
+        } intui;
+#endif
 #if defined(SDL_VIDEO_DRIVER_VIVANTE)
         struct
         {
