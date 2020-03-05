01f576659fdc103300470b2fd04540ac882f9c4a
BeWorld <36823759+BeWorld2018@users.noreply.github.com>
2020-03-05 16:09:19 +0100

    MOS : add AHIAUD_bootstrap

--- src/audio/SDL_sysaudio.h
+++ src/audio/SDL_sysaudio.h
@@ -208,6 +208,7 @@ extern AudioBootStrap ANDROIDAUDIO_bootstrap;
 extern AudioBootStrap PSPAUDIO_bootstrap;
 extern AudioBootStrap EMSCRIPTENAUDIO_bootstrap;
 extern AudioBootStrap AMIGAOS4AUDIO_bootstrap;
+extern AudioBootStrap AHIAUD_bootstrap;
 #endif /* SDL_sysaudio_h_ */
 
 /* vi: set ts=4 sw=4 expandtab: */
